e667fe9c43782642d54d3cd538386c86d1702ecd
Roman Dontsov <dontsovroman@gmail.com>
2017-11-24 18:53:49 +0200

    errorMessage added

--- back/express/index.js
+++ back/express/index.js
@@ -4,6 +4,20 @@ const express = require('express');
 const clients = require('../DB/clients')
 const app = express();
 
+function getDbResult(res){
+    return new Promise((resolve,reject) => {
+        if(res && res.rows && res.rows.length){
+            resolve(res.rows);
+        } else {
+            reject(dbError());
+        }
+    })
+}
+
+const dbError = () => ({
+    errorMessage:'something went wrong'
+})
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
@@ -11,20 +25,22 @@ app.get('/', (req, res) => {
 app.get('/clients',(req,res) => {
     clients
     .all()
-    .then(dbResult=>res.send(dbResult))
+    .then(getDbResult)
+    .then(result => res.send(result))
+    .catch(err => res.send(err))
 });
 
 app.get('/clients/new',(req,res) => {
     clients
     .new()
-    .then(dbResult=>res.send(dbResult))
+    .then(dbResult => res.send(dbResult))
 });
 
 app.get('/clients/:id', (req,res) => {
     const id = req.params.id;
     clients
         .byId(id)
-        .then(dbResult=>res.send(dbResult))
+        .then(dbResult => res.send(dbResult))
 });
 
 
