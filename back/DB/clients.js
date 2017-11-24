57660f3e48ac73f450e753d93b971e271327befe
Roman Dontsov <dontsovroman@gmail.com>
2017-11-24 19:17:32 +0200

    errorMessage added

--- back/DB/clients.js
+++ back/DB/clients.js
@@ -1,8 +1,20 @@
 const db = require('./index');
-// db.query('SELECT * FROM clients')
-//     .then(res=>console.log(res));
+const handler = res => {
+    return new Promise((resolve) => {
+        if(res && res.rows && res.rows.length){
+            resolve(res.rows);
+        }
+        })
+}
+const error = () => ({
+    errorMessage:'no clients'
+});
+const errorById = id => ({
+    errorMessage: 'no client with id:'+id
+})
+
 module.exports = {
-    all: () => db.query('SELECT * FROM clients'),
-    new: () => db.query('SELECT * FROM clients where is_read=false'),
-    byId: id => db.query('SELECT * FROM clients where id=$1',[id])
+    all: () => db.query('SELECT * FROM clients').then(handler).catch(error),
+    new: () => db.query('SELECT * FROM clients where is_read=false').then(handler).catch(error),
+    byId: id => db.query('SELECT * FROM clients where id=$1',[id]).then(handler).catch(errorById(id))
 };
--- back/express/index.js
+++ back/express/index.js
@@ -4,16 +4,6 @@ const express = require('express');
 const clients = require('../DB/clients')
 const app = express();
 
-function getDbResult(res){
-    return new Promise((resolve,reject) => {
-        if(res && res.rows && res.rows.length){
-            resolve(res.rows);
-        } else {
-            reject(dbError());
-        }
-    })
-}
-
 const dbError = () => ({
     errorMessage:'something went wrong'
 })
@@ -25,7 +15,6 @@ app.get('/', (req, res) => {
 app.get('/clients',(req,res) => {
     clients
     .all()
-    .then(getDbResult)
     .then(result => res.send(result))
     .catch(err => res.send(err))
 });
@@ -33,14 +22,16 @@ app.get('/clients',(req,res) => {
 app.get('/clients/new',(req,res) => {
     clients
     .new()
-    .then(dbResult => res.send(dbResult))
+    .then(result => res.send(result))
+    .catch(err => res.send(err))
 });
 
 app.get('/clients/:id', (req,res) => {
     const id = req.params.id;
     clients
         .byId(id)
-        .then(dbResult => res.send(dbResult))
+        .then(result => res.send(result))
+        .catch(err => res.send(err))
 });
 
 
