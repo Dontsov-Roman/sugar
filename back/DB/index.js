05a5733e530f43fba37f8145339d889a8d362fc5
Roman Dontsov <dontsovroman@gmail.com>
2017-11-24 17:37:55 +0200

    index

--- back/DB/index.js
+++ back/DB/index.js
@@ -3,4 +3,4 @@ const config = require('./config.json');
     // Client  = require('pg').Client;
 const pool = new Pool(config);
 pool.query('SELECT * from clients').then(res=>console.log(res));
-// pool.end();
+pool.end();
