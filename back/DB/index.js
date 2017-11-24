7f3e7be6bebb51bc0d4bafe8580d32fd21175f49
Roman Dontsov <dontsovroman@gmail.com>
2017-11-24 17:00:56 +0200

    Update

--- back/DB/index.js
+++ back/DB/index.js
@@ -2,5 +2,5 @@ const Pool = require('pg').Pool;
 const config = require('./config.json');
     // Client  = require('pg').Client;
 const pool = new Pool(config);
-pool.query('SELECT * from clients').then(res=>{console.log(res);});
+pool.query('SELECT * from clients').then(res=>console.log(res));
 // pool.end();
